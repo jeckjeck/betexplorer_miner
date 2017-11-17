da9caa4eed6860aabd5b3bbe425c54e2fd9c5b31
Joakim Bäcklund <you@example.com>
2017-11-17 22:01:07 +0100

    some code cleaning

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import datetime
+import re
 import requests
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import re
-import datetime
 import dbscript
+
+url_list = [
+    'http://www.betexplorer.com/handball/sweden/allsvenskan/',
+    'http://www.betexplorer.com/handball/sweden/allsvenskan-2016-2017/results/',
+    'http://www.betexplorer.com/handball/sweden/allsvenskan-2015-2016/results/',
+    'http://www.betexplorer.com/handball/sweden/elitserien-2015-2016/results/?stage=bwsO16No',
+    'http://www.betexplorer.com/handball/sweden/handbollsligan-2016-2017/results/?stage=YBwji1fj',
+    'http://www.betexplorer.com/handball/sweden/handbollsligan/results/']
+
+women_list = [
+    'http://www.betexplorer.com/handball/sweden/elitserien-women-2015-2016/?stage=6DT0aOMA',
+    'http://www.betexplorer.com/handball/sweden/she-women-2016-2017/?stage=rLgzCt1S',
+    'http://www.betexplorer.com/handball/sweden/she-women/']
 
 
 def get_soup(url):
@@ -42,20 +55,11 @@ def chrome():
     return driver
 
 
-def get_game_odds():
+def get_game_odds(list_of_urls=url_list):
     cur = dbscript.cur
     query = """DELETE FROM bet"""
     cur.execute(query)
-    url_list = ['http://www.betexplorer.com/handball/sweden/allsvenskan/',
-                'http://www.betexplorer.com/handball/sweden/allsvenskan-2016-2017/results/',
-                'http://www.betexplorer.com/handball/sweden/allsvenskan-2015-2016/results/',
-                'http://www.betexplorer.com/handball/sweden/elitserien-2015-2016/results/?stage=bwsO16No',
-                'http://www.betexplorer.com/handball/sweden/handbollsligan-2016-2017/results/?stage=YBwji1fj',
-                'http://www.betexplorer.com/handball/sweden/handbollsligan/results/',
-                'http://www.betexplorer.com/handball/sweden/elitserien-women-2015-2016/?stage=6DT0aOMA',
-                'http://www.betexplorer.com/handball/sweden/she-women-2016-2017/?stage=rLgzCt1S',
-                'http://www.betexplorer.com/handball/sweden/she-women/']
-    for url in url_list:
+    for url in list_of_urls:
         links = get_game(url=url)
         type_list = ["#ou", "#ah"]
         driver = chrome()
@@ -64,15 +68,20 @@ def get_game_odds():
                 link_ = str('http://www.betexplorer.com') + str(link) + str(line_type)
                 driver.get(link_)
                 soup = bs(driver.page_source, "html.parser")
-                even_line = get_lines(soup=soup)
+                even_line = get_lines_func(soup=soup)
                 teams = get_teams(soup=soup)
                 date = get_date(soup=soup)
+                if line_type == "#ou":
+                    home_res, away_res = get_result(soup)
                 if even_line is None:
-                    print(link_)
+                    try:
+                        print(link_)
+                    except ValueError:
+                        pass
                 if str(line_type) == "#ah":
-                    insert_into_db(date, teams, even_line, cur=cur, line_type="#ah")
+                    insert_into_db(date, teams, even_line, home_res, away_res, cur=cur, line_type="#ah")
                 else:
-                    insert_into_db(date, teams, even_line, cur=cur)
+                    insert_into_db(date, teams, even_line, home_res, away_res, cur=cur)
 
 
 def get_date(soup):
@@ -80,6 +89,11 @@ def get_date(soup):
     full_date = full_date.text
     date, time_ = full_date.split(' - ')
     return datetime.datetime.strptime(date, '%d.%m.%Y').strftime('%Y-%m-%d')
+
+
+def get_result(soup):
+    full_res = soup.find("p", {'id': 'js-score'})
+    return full_res.text.split(':')
 
 
 def get_teams(soup):
@@ -97,35 +111,32 @@ def calc_even_line(odds_diff: list, lines: list) -> float:
     return even_line
 
 
-def insert_into_db(date_, teams_, line_, cur, line_type=None):
+def insert_into_db(date_, teams_, line_, home, away, cur, line_type=None):
     if line_type == "#ah":
         query = "UPDATE bet SET spread=%s WHERE Date=%s and teams=%s"
         cur.execute(query, (line_, date_, teams_))
     else:
-        query = "INSERT INTO bet (Date, teams, total) VALUES (%s,%s,%s)"
-        cur.execute(query, (date_, teams_, line_))
+        query = "INSERT INTO bet (Date, teams, total, Result_h, Result_a, Result_t, Result_d) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        cur.execute(query, (date_, teams_, line_, int(home), int(away), int(home) + int(away), int(away) - int(home)),)
 
 
-def get_lines(soup):
+def get_bookie(soup):
     bookies = []
-    line_list = []
-    odds_list = []
-    odds_diff = []
-    bookie_lines = []
     links = soup.findAll("a")
-
     for link in links:
         span_links = link.findAll("span")
-        for x in span_links:
-            if x.get('title'):
-                bookies.append(x.get('title'))
+        [bookies.append(x.get('title')) for x in span_links if x.get('title')]
+    return bookies
+
+def get_lines(soup):
+    line_list = []
     lines = soup.findAll("td", {'table-main__doubleparameter'})
-    odds = soup.findAll("td", {'table-main__odds'})
     [line_list.append(float(x.text)) for x in lines]
-    try:
-        [odds_list.append(float(' '.join(re.findall(r'\d.\d+', x.text)))) for x in odds]
-    except ValueError:
-        return max(line_list, key=line_list.count)
+    return line_list
+
+def get_bookie_line(line_list, bookies, odds_list):
+    bookie_lines = []
+    odds_diff = []
     k = 0
     for i in range(len(line_list)):
         if i > 0 and line_list[i] != line_list[i - 1]:
@@ -135,6 +146,19 @@ def get_lines(soup):
             odds_diff.append(odds_list[2 * (i + k)] - odds_list[2 * (i + k) + 1])
             bookie_lines.append(line_list[i])
             # print(line_list[i], "diff",  diff)
+    return bookie_lines, odds_diff
+
+def get_lines_func(soup):
+    odds_list = []
+    bookies = get_bookie(soup)
+    line_list = get_lines(soup)
+    odds = soup.findAll("td", {'table-main__odds'})
+    try:
+        [odds_list.append(float(' '.join(re.findall(r'\d.\d+', x.text)))) for x in odds]
+    except ValueError:
+        return max(line_list, key=line_list.count)
+
+    bookie_lines, odds_diff = get_bookie_line(line_list, bookies, odds_list)
 
     if len(bookie_lines) > 1:
         # print("correct line", get_even_line(odds_diff, bookie_lines))
