4e6d52f4e112934d65bc7c2823765bbbe3d8200c
Joakim Bäcklund <jeckzor@hotmail.com>
2017-12-13 16:21:30 +0100

    Add files via upload

--- anton_mongodb_nba.R
+++ anton_mongodb_nba.R
@@ -5,13 +5,22 @@ library(RMongo)
 library(rjson)
 
 hostname = ""
-username = "username"
-password = "password"
+username = ""
+password = ""
 
-mongo <- mongoDbConnect("nba_standings", host= hostname)
-
+mongo <- mongoDbConnect("nba_standings", host=hostname)
 authenticated <- dbAuthenticate(mongo, username, password)
 output = RMongo::dbGetQuery(mongo,"dat","{}")
 
 real = lapply(output[,1], fromJSON)
 
+df <- data.frame(team=as.character(), wins=as.numeric(), losses=as.numeric(), stringsAsFactors = FALSE)
+
+
+for (i in 1:length(real)){
+  df[i,1] <- real[[i]]$team
+  df[i,2] <- real[[i]]$wins
+  df[i,3] <- real[[i]]$losses
+}
+
+df
